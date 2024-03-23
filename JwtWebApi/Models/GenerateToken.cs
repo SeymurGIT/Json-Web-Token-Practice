3b073ae2ab31f527765922961bbcec273b5f77d0
SeymurGIT <seymur.2004.78@mail.ru>
2024-03-23 15:57:27 +0400

    JWT Practice

---
+++ JwtWebApi/Models/GenerateToken.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtWebApi.Models
+{
+    public class GenerateToken
+    {
+        public string TokenGenerate()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapijwt");
+            
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore:DateTime.Now, expires:DateTime.Now.AddMinutes(2), signingCredentials: credentials);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+            
+        }
+        public string TokenGenerateAdmin()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreapijwt");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "Investor")
+
+            };
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddMinutes(3), signingCredentials:credentials, claims:claims);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(jwtSecurityToken);
+        }
+    }
+}
